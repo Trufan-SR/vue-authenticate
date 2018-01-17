de9600501741c0df23f1b7a16b949f971fb34390
Kieran Eglin <kieran.eglin@gmail.com>
2018-01-17 10:44:04 -0800

    forgot to build

--- dist/vue-authenticate.es2015.js
+++ dist/vue-authenticate.es2015.js
@@ -636,7 +636,7 @@ var defaultOptions = {
       authorizationEndpoint: 'https://api.twitch.tv/kraken/oauth2/authorize',
       redirectUri: window.location.origin,
       requiredUrlParams: ['scope'],
-      scope: ['user:read:email'],
+      scope: ['user:read:email', 'user_follows_edit', 'user_subscriptions'],
       scopeDelimiter: '+',
       oauthType: '2.0',
       popupOptions: { width: 500, height: 560 }

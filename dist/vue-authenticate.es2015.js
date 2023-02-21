a0bea63be703d579ffea4262df77a774c77679f9
Poe <poe@trufan.io>
2023-02-21 14:10:01 -0500

    use twitter authirize not authenticate

--- dist/vue-authenticate.es2015.js
+++ dist/vue-authenticate.es2015.js
@@ -508,22 +508,22 @@ Promise$1._setUnhandledRejectionFn = function _setUnhandledRejectionFn(fn) {
  */
 var defaultOptions = {
   baseUrl: null,
-  tokenName: 'token',
-  tokenPrefix: 'vueauth',
-  tokenHeader: 'Authorization',
-  tokenType: 'Bearer',
-  loginUrl: '/auth/login',
-  registerUrl: '/auth/register',
+  tokenName: "token",
+  tokenPrefix: "vueauth",
+  tokenHeader: "Authorization",
+  tokenType: "Bearer",
+  loginUrl: "/auth/login",
+  registerUrl: "/auth/register",
   logoutUrl: null,
-  storageType: 'localStorage',
-  storageNamespace: 'vue-authenticate',
+  storageType: "localStorage",
+  storageNamespace: "vue-authenticate",
   cookieStorage: {
     domain: window.location.hostname,
-    path: '/',
-    secure: false
+    path: "/",
+    secure: false,
   },
-  requestDataKey: 'data',
-  responseDataKey: 'data',
+  requestDataKey: "data",
+  responseDataKey: "data",
 
   /**
    * Default request interceptor for Axios library
@@ -535,12 +535,12 @@ var defaultOptions = {
     $auth.$http.interceptors.request.use(function (config) {
       if ($auth.isAuthenticated()) {
         config.headers[tokenHeader] = [
-          $auth.options.tokenType, $auth.getToken()
-        ].join(' ');
+          $auth.options.tokenType,
+          $auth.getToken() ].join(" ");
       } else {
         delete config.headers[tokenHeader];
       }
-      return config
+      return config;
     });
   },
 
@@ -551,212 +551,214 @@ var defaultOptions = {
   bindResponseInterceptor: function ($auth) {
     $auth.$http.interceptors.response.use(function (response) {
       $auth.setToken(response);
-      return response
+      return response;
     });
   },
 
   providers: {
     facebook: {
-      name: 'facebook',
-      url: '/auth/facebook',
-      authorizationEndpoint: 'https://www.facebook.com/v2.11/dialog/oauth',
-      redirectUri: window.location.origin + '/',
-      requiredUrlParams: ['display', 'scope'],
-      scope: ['email'],
-      scopeDelimiter: ',',
-      display: 'popup',
-      oauthType: '2.0',
-      popupOptions: { width: 580, height: 400 }
+      name: "facebook",
+      url: "/auth/facebook",
+      authorizationEndpoint: "https://www.facebook.com/v2.11/dialog/oauth",
+      redirectUri: window.location.origin + "/",
+      requiredUrlParams: ["display", "scope"],
+      scope: ["email"],
+      scopeDelimiter: ",",
+      display: "popup",
+      oauthType: "2.0",
+      popupOptions: { width: 580, height: 400 },
     },
 
     google: {
-      name: 'google',
-      url: '/auth/google',
-      authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+      name: "google",
+      url: "/auth/google",
+      authorizationEndpoint: "https://accounts.google.com/o/oauth2/auth",
       redirectUri: window.location.origin,
-      requiredUrlParams: ['scope'],
-      optionalUrlParams: ['display'],
-      scope: ['profile', 'email'],
-      scopePrefix: 'openid',
-      scopeDelimiter: ' ',
-      display: 'popup',
-      oauthType: '2.0',
-      popupOptions: { width: 452, height: 633 }
+      requiredUrlParams: ["scope"],
+      optionalUrlParams: ["display"],
+      scope: ["profile", "email"],
+      scopePrefix: "openid",
+      scopeDelimiter: " ",
+      display: "popup",
+      oauthType: "2.0",
+      popupOptions: { width: 452, height: 633 },
     },
 
     github: {
-      name: 'github',
-      url: '/auth/github',
-      authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+      name: "github",
+      url: "/auth/github",
+      authorizationEndpoint: "https://github.com/login/oauth/authorize",
       redirectUri: window.location.origin,
-      optionalUrlParams: ['scope'],
-      scope: ['user:email'],
-      scopeDelimiter: ' ',
-      oauthType: '2.0',
-      popupOptions: { width: 1020, height: 618 }
+      optionalUrlParams: ["scope"],
+      scope: ["user:email"],
+      scopeDelimiter: " ",
+      oauthType: "2.0",
+      popupOptions: { width: 1020, height: 618 },
     },
 
     instagram: {
-      name: 'instagram',
-      url: '/auth/instagram',
-      authorizationEndpoint: 'https://api.instagram.com/oauth/authorize',
+      name: "instagram",
+      url: "/auth/instagram",
+      authorizationEndpoint: "https://api.instagram.com/oauth/authorize",
       redirectUri: window.location.origin,
-      requiredUrlParams: ['scope'],
-      scope: ['basic'],
-      scopeDelimiter: '+',
-      oauthType: '2.0',
-      popupOptions: { width: null, height: null }
+      requiredUrlParams: ["scope"],
+      scope: ["basic"],
+      scopeDelimiter: "+",
+      oauthType: "2.0",
+      popupOptions: { width: null, height: null },
     },
 
     twitter: {
-      name: 'twitter',
-      url: '/auth/twitter',
-      authorizationEndpoint: 'https://api.twitter.com/oauth/authenticate',
+      name: "twitter",
+      url: "/auth/twitter",
+      authorizationEndpoint: "https://api.twitter.com/oauth/authorize",
       redirectUri: window.location.origin,
-      oauthType: '1.0',
-      popupOptions: { width: 495, height: 645 }
+      oauthType: "1.0",
+      popupOptions: { width: 495, height: 645 },
     },
 
     bitbucket: {
-      name: 'bitbucket',
-      url: '/auth/bitbucket',
-      authorizationEndpoint: 'https://bitbucket.org/site/oauth2/authorize',
-      redirectUri: window.location.origin + '/',
-      optionalUrlParams: ['scope'],
-      scope: ['email'],
-      scopeDelimiter: ' ',
-      oauthType: '2.0',
-      popupOptions: { width: 1020, height: 618 }
+      name: "bitbucket",
+      url: "/auth/bitbucket",
+      authorizationEndpoint: "https://bitbucket.org/site/oauth2/authorize",
+      redirectUri: window.location.origin + "/",
+      optionalUrlParams: ["scope"],
+      scope: ["email"],
+      scopeDelimiter: " ",
+      oauthType: "2.0",
+      popupOptions: { width: 1020, height: 618 },
     },
 
     linkedin: {
-      name: 'linkedin',
-      url: '/auth/linkedin',
-      authorizationEndpoint: 'https://www.linkedin.com/oauth/v2/authorization',
+      name: "linkedin",
+      url: "/auth/linkedin",
+      authorizationEndpoint: "https://www.linkedin.com/oauth/v2/authorization",
       redirectUri: window.location.origin,
-      requiredUrlParams: ['state'],
-      scope: ['r_emailaddress'],
-      scopeDelimiter: ' ',
-      state: 'STATE',
-      oauthType: '2.0',
-      popupOptions: { width: 527, height: 582 }
+      requiredUrlParams: ["state"],
+      scope: ["r_emailaddress"],
+      scopeDelimiter: " ",
+      state: "STATE",
+      oauthType: "2.0",
+      popupOptions: { width: 527, height: 582 },
     },
 
     live: {
-      name: 'live',
-      url: '/auth/live',
-      authorizationEndpoint: 'https://login.live.com/oauth20_authorize.srf',
+      name: "live",
+      url: "/auth/live",
+      authorizationEndpoint: "https://login.live.com/oauth20_authorize.srf",
       redirectUri: window.location.origin,
-      requiredUrlParams: ['display', 'scope'],
-      scope: ['wl.emails'],
-      scopeDelimiter: ' ',
-      display: 'popup',
-      oauthType: '2.0',
-      popupOptions: { width: 500, height: 560 }
+      requiredUrlParams: ["display", "scope"],
+      scope: ["wl.emails"],
+      scopeDelimiter: " ",
+      display: "popup",
+      oauthType: "2.0",
+      popupOptions: { width: 500, height: 560 },
     },
 
     twitch: {
-      name: 'twitch',
-      url: '/auth/twitch',
-      authorizationEndpoint: 'https://api.twitch.tv/kraken/oauth2/authorize',
+      name: "twitch",
+      url: "/auth/twitch",
+      authorizationEndpoint: "https://api.twitch.tv/kraken/oauth2/authorize",
       redirectUri: window.location.origin,
-      requiredUrlParams: ['scope'],
-      scope: ['user:read:email', 'user_follows_edit', 'user_subscriptions'],
-      scopeDelimiter: '+',
-      oauthType: '2.0',
-      popupOptions: { width: 500, height: 560 }
+      requiredUrlParams: ["scope"],
+      scope: ["user:read:email", "user_follows_edit", "user_subscriptions"],
+      scopeDelimiter: "+",
+      oauthType: "2.0",
+      popupOptions: { width: 500, height: 560 },
     },
 
     youtube: {
-      name: 'youtube',
-      url: '/auth/youtube',
-      authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+      name: "youtube",
+      url: "/auth/youtube",
+      authorizationEndpoint: "https://accounts.google.com/o/oauth2/auth",
       redirectUri: window.location.origin,
-      requiredUrlParams: ['scope'],
-      optionalUrlParams: ['display', 'access_type'],
-      scope: ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/userinfo.email'],
-      scopePrefix: 'openid',
-      scopeDelimiter: ' ',
-      accessType: 'offline',
-      display: 'popup',
-      oauthType: '2.0',
-      popupOptions: { width: 452, height: 633 }
+      requiredUrlParams: ["scope"],
+      optionalUrlParams: ["display", "access_type"],
+      scope: [
+        "https://www.googleapis.com/auth/youtube",
+        "https://www.googleapis.com/auth/userinfo.email" ],
+      scopePrefix: "openid",
+      scopeDelimiter: " ",
+      accessType: "offline",
+      display: "popup",
+      oauthType: "2.0",
+      popupOptions: { width: 452, height: 633 },
     },
 
     streamlabs: {
-      name: 'streamlabs',
-      url: '/auth/streamlabs',
-      authorizationEndpoint: 'https://streamlabs.com/api/v1.0/authorize',
+      name: "streamlabs",
+      url: "/auth/streamlabs",
+      authorizationEndpoint: "https://streamlabs.com/api/v1.0/authorize",
       redirectUri: window.location.origin,
-      requiredUrlParams: ['scope'],
-      scope: ['donations.create', 'donations.read'],
-      scopeDelimiter: ' ',
-      oauthType: '2.0',
-      popupOptions: { width: 1020, height: 700 }
+      requiredUrlParams: ["scope"],
+      scope: ["donations.create", "donations.read"],
+      scopeDelimiter: " ",
+      oauthType: "2.0",
+      popupOptions: { width: 1020, height: 700 },
     },
 
     streamtip: {
-      name: 'streamtip',
-      url: '/auth/streamtip',
-      authorizationEndpoint: 'https://streamtip.com/api/oauth2/authorize',
+      name: "streamtip",
+      url: "/auth/streamtip",
+      authorizationEndpoint: "https://streamtip.com/api/oauth2/authorize",
       redirectUri: window.location.origin,
-      oauthType: '2.0',
-      popupOptions: { width: 1020, height: 700 }
+      oauthType: "2.0",
+      popupOptions: { width: 1020, height: 700 },
     },
 
     stripe: {
-      name: 'stripe',
-      url: '/auth/stripe',
-      authorizationEndpoint: 'https://connect.stripe.com/oauth/authorize',
+      name: "stripe",
+      url: "/auth/stripe",
+      authorizationEndpoint: "https://connect.stripe.com/oauth/authorize",
       redirectUri: window.location.origin,
-      requiredUrlParams: ['response_type', 'client_id', 'scope'],
-      display: 'popup',
-      oauthType: '2.0',
-      popupOptions: { width: 500, height: 560 }
+      requiredUrlParams: ["response_type", "client_id", "scope"],
+      display: "popup",
+      oauthType: "2.0",
+      popupOptions: { width: 500, height: 560 },
     },
 
     mixer: {
-      name: 'mixer',
-      url: '/auth/mixer',
-      authorizationEndpoint: 'https://mixer.com/oauth/authorize',
+      name: "mixer",
+      url: "/auth/mixer",
+      authorizationEndpoint: "https://mixer.com/oauth/authorize",
       redirectUri: window.location.origin,
-      display: 'popup',
-      oauthType: '2.0',
-      popupOptions: { width: 500, height: 560 }
+      display: "popup",
+      oauthType: "2.0",
+      popupOptions: { width: 500, height: 560 },
     },
-    
+
     oauth1: {
       name: null,
-      url: '/auth/oauth1',
+      url: "/auth/oauth1",
       authorizationEndpoint: null,
       redirectUri: window.location.origin,
-      oauthType: '1.0',
-      popupOptions: null
+      oauthType: "1.0",
+      popupOptions: null,
     },
 
     oauth2: {
       name: null,
-      url: '/auth/oauth2',
+      url: "/auth/oauth2",
       clientId: null,
       redirectUri: window.location.origin,
       authorizationEndpoint: null,
-      defaultUrlParams: ['response_type', 'client_id', 'redirect_uri'],
+      defaultUrlParams: ["response_type", "client_id", "redirect_uri"],
       requiredUrlParams: null,
       optionalUrlParams: null,
       scope: null,
       scopePrefix: null,
       scopeDelimiter: null,
       state: null,
-      oauthType: '2.0',
+      oauthType: "2.0",
       popupOptions: null,
-      responseType: 'code',
+      responseType: "code",
       responseParams: {
-        code: 'code',
-        clientId: 'clientId',
-        redirectUri: 'redirectUri'
-      }
-    }
-  }
+        code: "code",
+        clientId: "clientId",
+        redirectUri: "redirectUri",
+      },
+    },
+  },
 };
 
 var CookieStorage = function CookieStorage(defaultOptions) {
